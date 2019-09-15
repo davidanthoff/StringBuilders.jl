af2e0ab9dab1e15972006b0f0ccdc62e37e62c8a
David Anthoff <anthoff@berkeley.edu>
2019-09-14 19:41:05 -0700

    Add coverage options

--- .github/actions/run-tests/lib/main.js
+++ .github/actions/run-tests/lib/main.js
@@ -17,8 +17,17 @@ core.debug(`platform: ${osPlat}`);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            const codecov = core.getInput('codecov');
+            const coveralls = core.getInput('coveralls');
             // Test if Julia has been installed by showing versioninfo()
-            yield exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; Pkg.build(); Pkg.test("StringBuilders")']);
+            yield exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; if VERSION >= v\"1.1.0-rc1\"; Pkg.build(verbose=true); else Pkg.build(); end']);
+            yield exec.exec('julia', ['--color=yes', '--check-bounds=yes', '--project', '-e', 'using Pkg; Pkg.test(coverage=true)']);
+            if (codecov == 'true') {
+                yield exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())']);
+            }
+            if (coveralls == 'true') {
+                yield exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())']);
+            }
         }
         catch (error) {
             core.setFailed(error.message);
--- .github/actions/run-tests/src/main.ts
+++ .github/actions/run-tests/src/main.ts
@@ -11,8 +11,20 @@ core.debug(`platform: ${osPlat}`)
 
 async function run() {
     try {
+        const codecov = core.getInput('codecov')
+        const coveralls = core.getInput('coveralls')
+
         // Test if Julia has been installed by showing versioninfo()
-        await exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; Pkg.build(); Pkg.test("StringBuilders")'])
+        await exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; if VERSION >= v\"1.1.0-rc1\"; Pkg.build(verbose=true); else Pkg.build(); end'])
+        await exec.exec('julia', ['--color=yes', '--check-bounds=yes', '--project', '-e', 'using Pkg; Pkg.test(coverage=true)'])
+
+        if(codecov=='true') {
+            await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
+        }
+
+        if(coveralls=='true') {
+            await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())'])
+        }
     } catch (error) {
         core.setFailed(error.message)
     }
