80fffa191b2e00bd5a69f7e7c4b1fb082fdb807a
David Anthoff <anthoff@berkeley.edu>
2019-09-14 19:45:34 -0700

    Disable coverage and coveralls for now

--- .github/actions/run-tests/lib/main.js
+++ .github/actions/run-tests/lib/main.js
@@ -22,12 +22,12 @@ function run() {
             // Test if Julia has been installed by showing versioninfo()
             yield exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; if VERSION >= v\"1.1.0-rc1\"; Pkg.build(verbose=true); else Pkg.build(); end']);
             yield exec.exec('julia', ['--color=yes', '--check-bounds=yes', '--project', '-e', 'using Pkg; Pkg.test(coverage=true)']);
-            if (codecov == 'true') {
-                yield exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())']);
-            }
-            if (coveralls == 'true') {
-                yield exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())']);
-            }
+            // if(codecov=='true') {
+            //     await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
+            // }
+            // if(coveralls=='true') {
+            //     await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())'])
+            // }
         }
         catch (error) {
             core.setFailed(error.message);
--- .github/actions/run-tests/src/main.ts
+++ .github/actions/run-tests/src/main.ts
@@ -18,13 +18,13 @@ async function run() {
         await exec.exec('julia', ['--color=yes', '--project', '-e', 'using Pkg; if VERSION >= v\"1.1.0-rc1\"; Pkg.build(verbose=true); else Pkg.build(); end'])
         await exec.exec('julia', ['--color=yes', '--check-bounds=yes', '--project', '-e', 'using Pkg; Pkg.test(coverage=true)'])
 
-        if(codecov=='true') {
-            await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
-        }
+        // if(codecov=='true') {
+        //     await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Codecov.submit(process_folder())'])
+        // }
 
-        if(coveralls=='true') {
-            await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())'])
-        }
+        // if(coveralls=='true') {
+        //     await exec.exec('julia', ['--color=yes', '-e', 'using Pkg; Pkg.add("Coverage"); using Coverage; Coveralls.submit(process_folder())'])
+        // }
     } catch (error) {
         core.setFailed(error.message)
     }
