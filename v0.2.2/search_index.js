bbcd13d238bb75e6da5194304617effc80537f9a
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-20 03:04:35 +0000

    build based on 6274bba

---
+++ v0.2.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#StringBuilders.jl-1","page":"Home","title":"StringBuilders.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"TODO: Write documentation","category":"page"}]
+}
