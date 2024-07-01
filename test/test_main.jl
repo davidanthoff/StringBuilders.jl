c23b74fcb7508e4a2005b647024618970d767f5f
David Anthoff <anthoff@berkeley.edu>
2024-07-01 17:31:36 +0200

    Fix tests

---
+++ test/test_main.jl
@@ -0,0 +1,24 @@
+
+@testitem "StringBuilders" begin
+    sb = StringBuilder()
+    append!(sb, "First string.")
+    append!(sb, "Second string.")
+    append!(sb, "Third string.")
+
+    s = String(sb)
+    @test s == "First string.Second string.Third string."
+    s2 = String(sb)
+    @test s2 == "First string.Second string.Third string."
+
+    append!(sb, "Fourth string.")
+    s3 = String(sb)
+
+    @test s3 == "First string.Second string.Third string.Fourth string."
+
+    b = StringBuilder()
+    append!(b, "1", "2", "3")
+    @test String(b) == "123"
+
+    sb = StringBuilder()
+    @test isempty(String(sb))
+end
