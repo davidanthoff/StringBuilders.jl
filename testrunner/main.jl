76af97d49bdca345ba0fcec650f6177fe58abaa3
David Anthoff <anthoff@berkeley.edu>
2024-07-05 12:42:30 +0200

    Fix a bug

--- testrunner/main.jl
+++ testrunner/main.jl
@@ -13,7 +13,7 @@ results = run_tests(
 for result in results
     if result.result.status!="yea"
         for message in result.result.message
-            println("::error file=$(TestItemRunner2.uri2filepath(message.location.uri)),line=$(message.location.range.start.line),endLine=$(message.location.range.stop.line),title=Error::$(message.message)")
+            println("::error file=$(TestItemRunner2.uri2filepath(TestItemRunner2.URI(message.location.uri))),line=$(message.location.range.start.line),endLine=$(message.location.range.stop.line),title=Error::$(message.message)")
         end
     end
 end
