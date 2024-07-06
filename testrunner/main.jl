f4b9762dbfb05f8905e6dc8a5be51438fdf39747
David Anthoff <anthoff@berkeley.edu>
2024-07-06 11:05:56 +0200

    Add multiline output

--- testrunner/main.jl
+++ testrunner/main.jl
@@ -3,6 +3,13 @@ println("We got args $ARGS")
 
 using TestItemRunner2
 
+function esc_data(s)
+    s = replace(s, '%' => "%25")
+    s = replace(s, '\r' => "%0D")
+    s = replace(s, '\n' => "%0A")
+    return s
+end
+
 results = run_tests(
     pwd(),
     environments=[TestEnvironment("Juliaup channel $i", Dict("JULIAUP_CHANNEL"=>i,"JULIA_DEPOT_PATH"=>joinpath(ARGS[1], "juliadepots/julia-$i"))) for i in ARGS[2:end]],
@@ -14,7 +21,7 @@ for result in results
     if result.result.status!="yea"
         if result.result.message!==missing
             for message in result.result.message
-                println("::error file=$(TestItemRunner2.uri2filepath(TestItemRunner2.URI(message.location.uri))),line=$(message.location.range.start.line+1),endLine=$(message.location.range.stop.line+1),title=Error::$(message.message)")
+                println("::error file=$(TestItemRunner2.uri2filepath(TestItemRunner2.URI(message.location.uri))),line=$(message.location.range.start.line+1),endLine=$(message.location.range.stop.line+1),title=Test failure::$(esc_data(message.message))")
             end
         end
     end
